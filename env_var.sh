af67a7402e4e3248aacf48ffc142eecd86ba1bf5
candiceT233 <mtang11@hawk.iit.edu>
2022-08-09 13:57:13 -0500

    mod for Ares env

--- env_var.sh
+++ env_var.sh
@@ -2,7 +2,7 @@
 
 #CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source /share/apps/python/miniconda3.7/etc/profile.d/conda.sh
+# source /share/apps/python/miniconda3.7/etc/profile.d/conda.sh
 
 #spack load hdf5 mochi-thallium catch2 glpk gflags glog
 #spack find --loaded
@@ -22,7 +22,7 @@ SPACK_DIR=$HOME/spack
 # Hermes running dirs -----------
 SCRIPT_DIR=$HOME/scripts/local-co-scheduling
 #CONF_NAME=hermes.conf
-CONF_NAME=hermes_single2.conf
+CONF_NAME=hermes_single3.conf
 #HERMES_INSTALL_DIR=`spack location -i hermes`
 HERMES_INSTALL_DIR=$INSTALL_DIR/hermes
 
@@ -45,4 +45,5 @@ RECORDER_REPO=$DL_DIR/Recorder-2.3.2
 RECORDER_INSTALL=$INSTALL_DIR/recorder
 
 #conda activate /files0/oddite/conda/ddmd/ # original global env
-conda activate hermes_ddmd # local
+#conda activate hermes_ddmd # local
+source hermes_ddmd/bin/activate
