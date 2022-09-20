890cae73accfdbb3affff3a74fd1053c36a9c4f4
candiceT233 <mtang11@hawk.iit.edu>
2022-09-20 15:03:43 -0500

    for loading deps from spack

--- load_hermes_deps.sh
+++ load_hermes_deps.sh
@@ -1,4 +1,3 @@
-#. ~/spack/share/spack/setup-env.sh
-spack load mochi-thallium@0.10.0 catch2@3.0.1 glpk glog
+spack load mochi-thallium@0.10.0 catch2@3.0.1 glpk glog yaml-cpp
 # spack load hdf5
 # spack load mpich
