1ae8ab81d4727d134f5969720b7286074fbcb8a4
candiceT233 <candicetang233@gamil.com>
2023-03-08 09:58:44 -0800

    updated directories

---
+++ src/aggregate_test.py
@@ -0,0 +1,142 @@
+import h5py
+import argparse
+import numpy as np
+from deepdrivemd.data.api import DeepDriveMD_API
+# from adios_prodcons import AdiosProducerConsumer
+
+import os # for env vars
+import sys # for final output to ostderr
+
+# add MPI for Hermes
+# from mpi4py import MPI
+# MPI.Init()
+# import ctypes
+# c_mpi_lib = ctypes.CDLL('./my_libs/c_mpi.so')
+# c_mpi_lib.c_mpi_init(None , None)
+
+# # SSD_PATH="/mnt/ssd/mtang11/"
+SSD_PATH=""
+if "DEV2_DIR" in os.environ:
+    SSD_PATH=os.environ.get('DEV2_DIR') + "/"
+
+def concatenate_last_n_h5(args):
+
+
+    fields = []
+    if args.no_rmsd is False:
+        fields.append("rmsd")
+    if args.no_fnc is False:
+        fields.append("fnc")
+    if args.no_contact_map is False:
+        fields.append("contact_map")
+    if args.no_point_cloud is False:
+        fields.append("point_cloud")
+
+    # Get list of input h5 files
+    api = DeepDriveMD_API(args.input_path)
+    # print(f"args.input_path = {args.input_path}")
+    md_data = api.get_last_n_md_runs()
+    files = md_data["data_files"]
+    # print(f"md_data = {md_data}")
+    # print(f"input files = {files}")
+
+    if args.verbose:
+        print(f"Collected {len(files)} h5 files.")
+
+    # Initialize data buffers
+    data = {x: [] for x in fields}
+
+    for in_file in files:
+        if args.verbose:
+            print("Reading", in_file)
+        
+        # driver="mpio", comm=MPI.COMM_WORLD
+        with h5py.File(in_file, "r") as fin:
+            # print(f"Datasetnames = {list(fin.keys())}")
+            # TODO: change readin style?
+            for field in fields:
+                data[field].append(fin[field][...])
+
+    # Concatenate data
+    for field in data:
+        data[field] = np.concatenate(data[field])
+
+    # Centor of mass (CMS) subtraction
+    if "point_cloud" in data:
+        if args.verbose:
+            print("Subtract center of mass (CMS) from point cloud")
+        cms = np.mean(
+            data["point_cloud"][:, 0:3, :].astype(np.float128), axis=2, keepdims=True
+        ).astype(np.float32)
+        data["point_cloud"][:, 0:3, :] -= cms
+        if args.dtype:
+            data['point_cloud'] = data['point_cloud'].astype(args.dtype)
+    
+    # TODO: change output dataset
+    # Open output filedriver="sec2" # ,driver="mpio", comm=MPI.COMM_WORLD
+    fout = h5py.File(args.output_path, "w", libver="latest") #rdcc_nbytes=398458880
+    
+    # Create new dsets from concatenated dataset
+    for field, concat_dset in data.items():
+        if field == "traj_file":
+            utf8_type = h5py.string_dtype("utf-8")
+            fout.create_dataset("traj_file", data=concat_dset, dtype=utf8_type)
+            continue
+
+        shape = concat_dset.shape
+        chunkshape = (1,) + shape[1:]
+        # print(f'{field} chunkshape:{chunkshape} ')
+        # Create dataset
+        cm_chunk=(400,)
+        pc_chunk=(400,3,200)
+        if concat_dset.dtype != object: #np.object:
+            # point_cloud-chunkshape : (1, 3, 200)
+            
+            if np.any(np.isnan(concat_dset)):
+                raise ValueError("NaN detected in concat_dset.")
+            dset = fout.create_dataset(
+                field, shape, dtype=concat_dset.dtype, chunks=pc_chunk
+            )
+        else:
+            # contact_map-chunkshape : (1,)
+            dset = fout.create_dataset(
+                field, shape, dtype=h5py.vlen_dtype(np.int16), #chunks=cm_chunk
+            )
+        # write data
+        dset[...] = concat_dset[...]
+
+    # Clean up
+    fout.flush()
+    fout.close()
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-no_pc', '--no-point_cloud', action='store_true', help='Skip to collect "point_cloud" dataset')
+    parser.add_argument('-no_cm', '--no-contact_map', action='store_true', help='Skip to collect "contact_map" dataset')
+    parser.add_argument('-no_rmsd', '--no-root-mean-square-deviation', dest='no_rmsd', action='store_true', help='Skip to collect "rmsd" dataset')
+    parser.add_argument('-no_fnc', '--no-fraction_of_contacts', dest='no_fnc', action='store_true', help='Skip collect "fnc" dataset')
+    parser.add_argument("--input_path")
+    '''parser.add_argument(
+            'input', metavar='N', type=str, nargs='+', help='h5 file(s)'
+    )'''
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--output_path')
+    parser.add_argument('--dtype', help='output dtype to cast')
+    # parser.add_argument('--adios', help='read adios "bp" files to aggregate')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+    concatenate_last_n_h5(args)
+
+    print("aggregate.py finished ...", file=sys.stderr)
+
+    # # Add MPI for Hermes
+    # MPI.Finalize()
+    # c_mpi_lib.c_mpi_finalize()
+    
---
+++ src/sim_emulator_test.py
@@ -0,0 +1,300 @@
+import scipy.sparse
+import argparse
+import numpy as np
+import h5py
+from pathlib import Path
+# from adios_prodcons import AdiosProducerConsumer
+from multiprocessing import Pool
+import time
+try:
+    import MDAnalysis as mda
+except:
+    mda = None
+    
+import os # for env vars
+import sys # for final output to ostderr
+
+# clear cache for test 
+# import streamlit as st
+# st.experimental_memo.clear()
+# st.experimental_singleton.clear()
+
+# # add MPI for io processes
+# from mpi4py import MPI
+# import mpi4py
+# mpi4py.rc(initialize=False, finalize=False)
+# mpi4py.MPI.Init()
+# import ctypes
+# c_mpi_lib = ctypes.CDLL('./my_libs/c_mpi.so')
+# c_mpi_lib.c_mpi_init(None , None)
+
+# # SSD_PATH="/mnt/ssd/mtang11/"
+SSD_PATH=""
+if "DEV2_DIR" in os.environ:
+    SSD_PATH=os.environ.get('DEV2_DIR') + "/"    
+    # os.system('gcc -print-file-name=libmpi.so')
+    # print(os.environ.get('HERMES_CLIENT'))
+    # exit()
+
+class SimEmulator:
+
+    def __init__(self, 
+            n_residues = 50, 
+            n_atoms = 500, 
+            n_frames = 100, 
+            n_jobs = 1):
+
+        self.n_residues = n_residues
+        self.n_atoms = n_atoms
+        self.n_frames = n_frames
+        self.n_jobs = n_jobs
+        self.nbytes = 0
+        self.universe = None
+
+    # def set_adios(self, sst, bp):
+
+    #     self.adios_on = sst or bp
+    #     if self.adios_on is True:
+    #         self.adios_init(sst, bp)
+
+    # def adios_init(self, sst=True, bp=True):
+
+    #     adios = AdiosProducerConsumer()
+    #     adios.set_engine({"sst":sst})
+    #     adios.set_engine({"bp":bp})
+    #     adios.setup_conn()
+    #     self.adios = adios
+
+    def contact_map(self, density=None, dtype='int16'):
+
+        if not self.is_contact_map:
+            return None
+
+        if density is None:
+            density = np.random.uniform(low=0.23, high=.235, size=(1,))[0]
+        S = scipy.sparse.random(self.n_residues, self.n_residues, density=density, dtype=dtype)
+        row = S.tocoo().row.astype(dtype)
+        col = S.tocoo().col.astype(dtype)
+
+        self.nbytes += row.nbytes
+        self.nbytes += col.nbytes
+
+        return [row, col]
+
+    def contact_maps(self):
+        if not self.is_contact_map:
+            return None
+        
+        cms = [ self.contact_map() for x in range(self.n_frames) ] 
+        r = [np.concatenate(x) for x in cms]
+        ret = np.empty(len(r), dtype=object)
+        ret[...] = r
+        return ret
+
+    def point_cloud(self, dtype='float32'):
+        
+        if not self.is_point_cloud:
+            return None
+
+        r = np.random.randn(3, self.n_residues).astype(dtype)
+        self.nbytes += r.nbytes
+        return r
+
+    def point_clouds(self):
+        if not self.is_point_cloud:
+            return None
+        pcs = [ self.point_cloud() for x in range(self.n_frames) ]
+        return pcs
+
+    def h5file(self, data, ds_name, fname=None):
+        mode = "a"
+
+        if fname is None:
+            fname = "{}.hdf5".format(self.output_filename) # original .h5
+
+        if isinstance(data, list):
+            dtype = data[0].dtype
+        elif data.dtype == object:
+            dtype = h5py.vlen_dtype(np.dtype(data[0].dtype))
+        
+        # if not os.path.exists(fname):
+        #     mode = "w" # problem using append
+        
+        # driver="sec2" #default
+        # if ds_name == 'point_cloud':
+        #     driver="mpio" # need build with mpi support
+        # driver="sec2"
+        chunkshape = (1,)
+        if ds_name == 'point_cloud':
+            chunkshape = (1, 3, 200)
+
+        # 100 chunks
+        dims = (400,)
+        if ds_name == 'point_cloud':
+            dims = (400,3,200)
+        
+        # TODO: change data creation layout # driver="mpio", comm=MPI.COMM_WORLD
+        with h5py.File(fname, mode, swmr=False) as h5_file:
+            if ds_name in h5_file:
+                del h5_file[ds_name]
+            h5_file.create_dataset(
+                        ds_name,
+                        data=data,
+                        dtype=dtype,
+                        # chunks=dims,
+                        )
+                
+
+    def trajectory(self):
+        coordinates = np.random.rand(self.n_atoms, 3)
+        return coordinates
+
+    def trajectories(self):
+        ret = [ self.trajectory() for x in range(self.n_frames) ]
+        return ret
+
+    def dcdfile(self, coordinates, fname=None, u=None):
+
+        if mda is None:
+            return
+
+        if fname is None:
+            fname = "{}.dcd".format(self.output_filename)
+
+        if u is None:
+            if self.universe:
+                u = self.universe
+            else:
+                u = mda.Universe.empty(n_atoms=self.n_atoms)
+                self.universe = u
+
+        w = mda.coordinates.DCD.DCDWriter(fname, self.n_atoms)
+        for c in coordinates:
+            u.load_new(c)
+            w.write(u.trajectory)
+        w.close()
+
+        return u
+
+    def pdbfile(self, structure, fname=None):
+
+        if fname is None:
+            fname = "{}.pdb".format(self.output_filename)
+
+        #TBD
+        Path(fname).touch()
+
+    def output_settings(self, 
+            output_filename=None, 
+            is_contact_map=True, 
+            is_point_cloud=True,
+            is_rmsd=True, 
+            is_fnc=True):
+        if output_filename is None:
+           self.output_filename = "residue_{}".format(self.n_residues)
+        self.is_contact_map = is_contact_map
+        self.is_point_cloud = is_point_cloud
+        self.is_rmsd = is_rmsd
+        self.is_fnc = is_fnc
+
+
+def user_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--residue', type=int, required=True)
+    parser.add_argument('-a', '--atom', type=int)
+    parser.add_argument('-f', '--frame', default=100, type=int)
+    parser.add_argument('-n', '--number_of_jobs', default=1, type=int)
+    parser.add_argument('--fnc', default=True)
+    parser.add_argument('--rmsd', default=True)
+    parser.add_argument('--contact_map', default=False) # =False to try MPIIO to work, default=True
+    parser.add_argument('--point_cloud', default=True)
+    parser.add_argument('--trajectory', default=False)
+    parser.add_argument('--output_filename', default=None)
+    parser.add_argument('--adios-sst', action='store_true', default=False)
+    parser.add_argument('--adios-bp', action='store_true', default=False)
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+
+    args = user_input()
+    obj = SimEmulator(n_residues = args.residue,
+            n_atoms = args.atom,
+            n_frames = args.frame,
+            n_jobs= args.number_of_jobs)
+
+    # obj.set_adios(args.adios_sst, args.adios_bp)
+
+    obj.output_settings(output_filename = args.output_filename,
+            is_contact_map = args.contact_map,
+            is_point_cloud = args.point_cloud,
+            is_rmsd = args.rmsd,
+            is_fnc = args.fnc)
+
+    def runs(i):
+        times = []
+        task_dir = SSD_PATH + "molecular_dynamics_runs/stage0000/task{:04d}/".format(i)
+        Path(task_dir).mkdir(parents=True, exist_ok=True)
+        
+        cms = obj.contact_maps()
+        if cms is not None:
+            obj.h5file(cms, 'contact_map', task_dir + obj.output_filename + ".h5")# + f"_ins_{i}.h5")
+            
+        pcs = obj.point_clouds()
+        if pcs is not None:
+            obj.h5file(pcs, 'point_cloud', task_dir + obj.output_filename + ".h5")#f"_ins_{i}.h5")
+
+        
+        # if obj.adios_on is True:
+        #     # reset file open by task id
+        #     obj.adios.close_conn()
+        #     obj.adios.file_path = task_dir + os.path.basename(obj.adios.file_path)
+        #     obj.adios.stream_path = task_dir + os.path.basename(obj.adios.stream_path)
+        #     obj.adios.setup_conn()
+        #     times.append(time.time())
+        #     if cms is not None:
+        #         obj.adios.put({'contact_map': cms})
+        #     if pcs is not None:
+        #         obj.adios.put({'point_cloud': pcs})
+        #     times.append(time.time())
+        # else:
+        
+        
+
+            
+            
+        
+    #
+        # if obj.adios_on is True:
+        #     obj.adios.close_conn()
+        dcd = obj.trajectories()
+        if dcd is not None:
+            obj.dcdfile(dcd, task_dir + obj.output_filename + ".dcd")#f"_ins_{i}.dcd")
+        obj.pdbfile(None, task_dir + "dummy.pdb")#obj.output_filename + ".pdb")
+
+        #print (max(times) - min(times), min(times), max(times) ) 
+        return task_dir, obj.nbytes
+
+    files = []
+    fbytes = 0
+    for i in range(obj.n_jobs):
+        fname, fbyte = runs(i)
+        fbytes += fbyte
+
+    # with Pool(obj.n_jobs) as p:
+        # res = (p.map(runs, list(range(obj.n_jobs))))
+    # files = []
+    # fbytes = 0
+    # for fname, fbyte in res:
+    #     files.append(fname)
+    #     fbytes += fbyte
+
+    print("total bytes written:{} in {} file(s)".format(fbytes, obj.n_jobs), file = sys.stderr)
+    # obj.adios.close_conn() if obj.adios_on else None
+
+    # # print("Error", file = sys.stderr )
+    # # Add MPI for Hermes
+    # MPI.Finalize()
+    # c_mpi_lib.c_mpi_finalize()
