cd1f74cb12680c46f8d2a52c9e9df557c248eb82
candiceT233 <candicetang233@gamil.com>
2023-03-12 11:27:01 -0700

    specify bluesky spack env

---
+++ bluesky_slurm_scripts/load_hermes_deps.sh
@@ -0,0 +1,11 @@
+. ~/spack/share/spack/setup-env.sh
+spack load --only dependencies hermes
+
+
+#spack load mochi-thallium@0.10.0 catch2@3.0.1 glpk glog yaml-cpp mpich #automake
+# spack load boost mochi-thallium@0.8.3 catch2@3.0.1 glpk glog yaml-cpp mpich 
+
+# spack load hermes
+# spack load hdf5
+# spack load mpich
+#spack load libbsd
